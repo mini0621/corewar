3951f0501a3dc45dd9f734b8cf908e407989973e
Sunah KIM <kosah302@gmail.com>
2019-06-05 11:52:08 +0200

    tkn _ creat..ing

--- tkn_create_func1.c
+++ tkn_create_func1.c
@@ -3,21 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   tkn_create_func1.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: allefebv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sunakim <sunakim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 10:08:01 by allefebv          #+#    #+#             */
-/*   Updated: 2019/06/04 15:20:06 by allefebv         ###   ########.fr       */
+/*   Updated: 2019/06/05 11:51:20 by sunakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "corewar.h"
+
 void	tkn_label(char *buff, t_pos *pos, t_lbl *labels, t_tkn *token)
 {
-	t_lbl	*tmp;
+	t_lbl	*tmp_l;
+	t_tkn	*tmp_t;
 	char	*name;
 
-	tmp = labels;
 	name = ft_strndup(buff + tkn->buff_start, tkn->buff_end - tkn->buff_start);
-	while (tmp != NULL && !ft_strequ(tmp->name != name)
+	while (tmp_l != NULL && !ft_strequ(tmp_l->name, name))
+		tmp_l = tmp_l->next;
+	if (tmp_l != NULL)
+	{
+		if (tmp_l->type == 'D')
+			ERROR
+		else
+		{
+			tmp_t = tmp_l->params;
+			tmp_l->type = 'D';
+			tmp_l->lc_label = pos->lc_instruction;
+			while (tmp_t != NULL)
+			{
+				ft_fill_gap_bytebuff(tmp_t, tmp_l); // to code + verify that the LC points to where the label we just received is pointing to
+				tmp_t = tmp_t->next;
+			}
+		}
+	}
+	else
+	{
+		tmp_l = (t_lbl *)ft_memalloc(sizeof(t_lbl));
+		tmp_l->name = name;
+		tmp_l->type = 'D';
+		tmp_l->lc_label = pos->lc_instruction; // not sure about the pos->lc_instruction
+		tmp_l->params = NULL;
+		ft_lstadd(&labels, ft_lstnew(tmp_l, sizeof(t_lbl)));
+	}
+
 	IF LABEL EXISTS IN SYMBOL TABLE
 		IF LABEL HAS TYPE DEFINED
 			THEN : ERROR
