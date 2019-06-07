2c998d3e64f3342f61393db1e8ac42ef1a793600
Alban <alban.lfbv@gmail.com>
2019-06-07 18:46:58 +0200

    libftprintf files ok

---
+++ libftprintf/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnishimo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/21 16:28:33 by mnishimo          #+#    #+#             */
+/*   Updated: 2019/06/04 20:45:15 by mnishimo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*ps;
+	unsigned char	*pd;
+	size_t			i;
+
+	i = 0;
+	if (!dst || !src || !n)
+		return (dst);
+	pd = (unsigned char *)dst;
+	ps = (unsigned char *)src;
+	while (i < n)
+	{
+		pd[i] = ps[i];
+		i++;
+	}
+	return (dst);
+}
