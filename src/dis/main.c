2dd2ae25f5b190014174c6f7d7c7dc4a3d438ab1
Misaki NISHIMOTO <mnishimo@e1r2p2.42.fr>
2019-07-02 10:42:31 +0200

    dis leaks of t_visu fixed

--- src/dis/main.c
+++ src/dis/main.c
@@ -6,7 +6,7 @@
 /*   By: mndhlovu <mndhlovu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 04:18:12 by mndhlovu          #+#    #+#             */
-/*   Updated: 2019/07/02 10:05:32 by mndhlovu         ###   ########.fr       */
+/*   Updated: 2019/07/02 10:41:35 by mnishimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,9 @@
 
 static void			dis_init_state(t_dis_game *game, t_game *vgame)
 {
-	game->deb_state = 0;
-	game->nbr_champs = 0;
-	game->visu_state = 0;
-	game->deb_state = 0;
-	game->pv_number = 0;
-	game->length = 0;
-	game->store = NULL;
-    if (!(game->visu = (t_visu *)malloc(sizeof(t_visu))))
-        return ;
+	ft_bzero(game, sizeof(t_dis_game));
+	if (!(game->visu = (t_visu *)malloc(sizeof(t_visu))))
+		return ;
 	ft_bzero(game->visu, sizeof(t_visu));
 	ft_bzero(vgame, sizeof(t_game));
 	ft_bzero(&(game->dismemdump[0]), CHAMP_MAX_SIZE * sizeof(t_ucc));
@@ -61,7 +55,7 @@ static int			dis_init_parser(int ac, char **av, t_dis_game *game)
 static int			dis_extras(t_dis_game *game)
 {
 	int				pl_num;
-	
+
 	pl_num = dis_load_player(game);
 	if (pl_num == -1)
 		return (-1);
@@ -73,42 +67,42 @@ static int			dis_extras(t_dis_game *game)
 
 int                 main(int ac, char **av)
 {
-    t_dis_game      game;
+	t_dis_game      game;
 	t_game			vgame;
-    int             pl_num;
-    int             flag;
+	int             pl_num;
+	int             flag;
 
-    flag = 1;
-    if (ac < 2)
-        return (dis_catch_error(US_ERROR, NULL));
-    dis_init_state(&game, &vgame);
-    if (!dis_init_parser(ac, av, &game))
-        return (0);
-    if (!game.deb_state && !game.visu_state)
-    {
-        while ((pl_num = dis_extras(&game)) != -1)
-            if (!(dis_write_output(&game, pl_num)))
-                return (0);
-    }
-    if (game.deb_state && !game.visu_state)
-    {
-        ft_printf("\t\tDEBUG MODE\t\t\n");
-        while ((pl_num = dis_extras(&game)) != -1)
-        {
-            if (!(dis_write_output(&game, pl_num)))
-                return (0);
-            dis_debug(&game, pl_num);
-        }
-    }
-    if (game.visu_state && !game.deb_state)
-    {
-        pl_num = dis_extras(&game);
-        if (pl_num != -1)
-        {
-            while (flag)
-                dis_output(&game, pl_num, &flag);
-        }  
-    }
-    dis_end_visu(game.visu);
-    return (0);
+	flag = 1;
+	if (ac < 2)
+		return (dis_catch_error(US_ERROR, NULL));
+	dis_init_state(&game, &vgame);
+	if (!dis_init_parser(ac, av, &game))
+		return (free_all(&game));
+	if (!game.deb_state && !game.visu_state)
+	{
+		while ((pl_num = dis_extras(&game)) != -1)
+			if (!(dis_write_output(&game, pl_num)))
+				return (free_all(&game));
+	}
+	if (game.deb_state && !game.visu_state)
+	{
+		ft_printf("\t\tDEBUG MODE\t\t\n");
+		while ((pl_num = dis_extras(&game)) != -1)
+		{
+			if (!(dis_write_output(&game, pl_num)))
+				return (free_all(&game));
+			dis_debug(&game, pl_num);
+		}
+	}
+	if (game.visu_state && !game.deb_state)
+	{
+		pl_num = dis_extras(&game);
+		if (pl_num != -1)
+		{
+			while (flag)
+				dis_output(&game, pl_num, &flag);
+		}  
+	}
+	dis_end_visu(game.visu);
+	return (free_all(&game));
 }
